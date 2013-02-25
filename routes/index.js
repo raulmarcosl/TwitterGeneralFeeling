8852d8044098fb6bf09c1bae9474b29b7a40159e
Raul Marcos <raulmarcosl@gmail.com>
2013-02-25 18:17:44 +0100

    Method that was used to process the lexicon

--- routes/index.js
+++ routes/index.js
@@ -38,8 +38,36 @@ function lematizacion(arrayTweets) {
 exports.showTweets = function(req, res){
 
 
+    // this method was used to process the lexicon, in order to get a separated terms list.
+    var tweets, negTerms = [], posTerms = "", negDoubleTerms = "", posDoubleTerms = "", ambiguousTerms = "";
 
-    var tweets;
+    require('fs')
+    .readFileSync('./public/static/lexicon.txt')
+    .toString()
+    .split('\n')
+    .forEach(function (line) {
+        var parts;
+
+        parts = line.split(/[\s]+/);
+
+        if (parts.length === 3) {
+            if (parts[2] === 'pos') {
+                posTerms += "\"" + parts[0] + "\", ";
+            } else if (parts[2] === 'neg'){
+                negTerms += "\"" + parts[0] + "\", ";
+            }
+
+        } else if (parts.length === 4) {
+
+            if (parts[2] === 'pos' && parts[3] === 'pos') {
+                posDoubleTerms += "\"" + parts[0] + "\", ";
+            } else if (parts[2] === 'neg' && parts[3] === 'neg') {
+                negDoubleTerms += "\"" + parts[0] + "\", ";
+            } else { //pos-neg or neg-pos
+                ambiguousTerms += "\"" + parts[0] + "\", ";
+            }
+        }
+    })
 
     res.render('index', { tweets: tweets, title: "Best page ever" });
 };
