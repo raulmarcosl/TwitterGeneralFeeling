44be991343c228e83cc8ffa5a033ee3bf1af662e
manutero <manutero@gmail.com>
2013-02-27 19:28:25 +0100

    starting working on the view \n basic design: div wrapper => div content => tweet-list : chart

--- routes/index.js
+++ routes/index.js
@@ -87,6 +87,27 @@ exports.showTweets = function(req, res){
         }
     })
 
-    res.render('index', { tweets: tweets, title: "Best page ever" });
+
+    // DEBUG: variable temporal solo para trastear con la vista 
+    tweets = [
+        {
+            "favorited": false,
+            "created_at": "Mon Jun 27 19:32:19 +0000 2011",
+            "text": "Misleading error message - If you try to follow a user who was recently suspended, you may see an error... http://t.co/cCIWIwg",
+        }
+        ,{
+            "favorited": false,
+            "created_at": "Mon Jun 27 01:21:23 +0000 2011",
+            "text": "Tweet delivery delays for streaming clients - We experienced temporary delays but the issue has now been... http://t.co/IcQut5R",
+        }
+        ,{
+            "favorited": false,
+            "created_at": "Mon Jun 27 01:21:23 +0000 2011",
+            "text": "This is just a test",
+        }
+    ]
+    // ---
+
+    res.render('index', { tweets: tweets, title: "Twitter General Feeling Analizer", percentage: 55 });
 };
 
